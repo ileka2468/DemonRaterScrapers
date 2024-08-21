a5caf7a130ad0161c4a6a9062c68c7651e5110e4
William <ileka2468@gmail.com>
2024-08-21 16:21:10 -0500

    styling changes

--- scrapeCoursessTaught.py
+++ scrapeCoursessTaught.py
@@ -10,10 +10,10 @@ from Wrappers.Professors import Professors
 driver = Chrome()
 wait = WebDriverWait(driver, 10)
 
-def processProfessor(professorId, facultyId):
+
+def process_professor(professorId, facultyId):
     url = f"https://www.cdm.depaul.edu/Faculty-and-Staff/Pages/faculty-info.aspx?fid={facultyId}"
     driver.get(url)
-    wait = WebDriverWait(driver, 10)
     wait.until(ec.presence_of_element_located((By.CLASS_NAME, "Faculty--Courses")))
     courses = driver.find_element(By.CLASS_NAME, "Faculty--Courses")
     course_elements = courses.find_element(By.ID, "facultyCourses").find_elements(By.CLASS_NAME, "facultyCourse")
@@ -22,25 +22,22 @@ def processProfessor(professorId, facultyId):
         pid = professorId
         cid = Courses.get_single_record({Courses.Cols.CODE: course_code}, Courses.Cols.COURSE_ID)[Courses.Cols.COURSE_ID]
         print(pid, cid)
+
         if not cid:
             continue
-        print(pid,cid)
+
+        print(pid, cid)
         CoursesTaught.insert({
             CoursesTaught.Cols.Foreign.PROFESSOR_ID: pid,
             CoursesTaught.Cols.Foreign.COURSE_CODE_ID: cid
         })
 
 
-
-
 def main():
     professors = Professors.get_all(Professors.Cols.PROFESSOR_ID, Professors.Cols.FACULTY_ID)
     for professor in professors:
         print(professor)
-        processProfessor(professor[Professors.Cols.PROFESSOR_ID],professor[Professors.Cols.FACULTY_ID])
-
-
-
+        process_professor(professor[Professors.Cols.PROFESSOR_ID], professor[Professors.Cols.FACULTY_ID])
 
 
 if __name__ == '__main__':
