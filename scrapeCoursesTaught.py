cd51b9dca55a96dbeb754effbb74dd19eed154f9
William <ileka2468@gmail.com>
2024-08-21 16:28:43 -0500

    cleanup repo

--- scrapeCoursesTaught.py
+++ scrapeCoursesTaught.py
@@ -20,17 +20,15 @@ def process_professor(professorId, facultyId):
     for course in course_elements:
         course_code = course.find_elements(By.TAG_NAME, "span")[0].get_attribute("innerText")
         pid = professorId
-        cid = Courses.get_single_record({Courses.Cols.CODE: course_code}, Courses.Cols.COURSE_ID)[Courses.Cols.COURSE_ID]
-        print(pid, cid)
+        result = Courses.get_single_record({Courses.Cols.CODE: course_code}, Courses.Cols.COURSE_ID)
+        if result:
+            cid = result[Courses.Cols.COURSE_ID]
+            print(pid, cid)
 
-        if not cid:
-            continue
-
-        print(pid, cid)
-        CoursesTaught.insert({
-            CoursesTaught.Cols.Foreign.PROFESSOR_ID: pid,
-            CoursesTaught.Cols.Foreign.COURSE_CODE_ID: cid
-        })
+            CoursesTaught.insert({
+                CoursesTaught.Cols.Foreign.PROFESSOR_ID: pid,
+                CoursesTaught.Cols.Foreign.COURSE_CODE_ID: cid
+            })
 
 
 def main():
